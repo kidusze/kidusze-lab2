6fdaababf5262506c9683b31da91aa5600445612
Kidus Zerihun <kiduszerihun@Kiduss-MacBook-Pro.local>
2024-10-06 19:39:43 -0400

    finished correcting the implementation for Thing.java

--- TypeC.java
+++ TypeC.java
@@ -1,9 +1,1 @@
-public class TypeC extends Thing{
-    private int count = 0;
-    public TypeC(int row, int col) {
-        super(row,col,'c');
-    }
-    public void maybeTurn() {
-        
-    }
-}
+
