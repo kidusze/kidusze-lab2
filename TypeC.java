891f3b18a51db2094e6aa663771b7bd1729198d2
Kidus Zerihun <kiduszerihun@Kiduss-MacBook-Pro.local>
2024-10-06 19:56:09 -0400

    implemented Type A,B and C

--- TypeC.java
+++ TypeC.java
@@ -1,1 +1,32 @@
+import java.util.Random;
+
+
+public class TypeC extends Thing{
+   private int count = 0;;
+   protected Random rand = new Random(System.currentTimeMillis());
+
+
+
+
+   public TypeC(int row, int col) {
+       super(row,col,'y');
+   }
+   public void maybeTurn(Random rand) {
+       
+       count++;
+       // Turn every 5 rounds
+       if (count == 5) {
+           count = 0;
+           int i = rand.nextInt(3);
+           if (i == 1) {
+               rightTurn();
+           }
+           else if (i == 2) {
+               leftTurn();
+           }
+       }
+      
+          
+   }
+}
 
